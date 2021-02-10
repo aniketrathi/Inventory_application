1673c20c1350dc01610edd54ce1301b6dda4e1dd
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-10 14:54:24 -0500

    category form issue fixed

--- routes/catalog.js
+++ routes/catalog.js
@@ -11,12 +11,14 @@ router.get("/", category_controller.index);
 
 router.get("/categories", category_controller.category_list);
 
-router.get("/category/:id", category_controller.category_detail);
-
 router.get("/category/create", category_controller.category_create_get);
 
+router.get("/category/:id", category_controller.category_detail);
+
+
+
 router.post(
-  "category/create",
+  "/category/create",
   category_validator.generateValidator,
   category_controller.category_create_post
 );
